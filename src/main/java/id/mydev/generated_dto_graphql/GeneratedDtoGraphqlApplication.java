5b49a6668e25c38576f170e314ecca717834babf
Muhammad Yusuf Alpian <llawliet1945@gmail.com>
2025-09-21 15:18:33 +0700

    initial commit

---
+++ src/main/java/id/mydev/generated_dto_graphql/GeneratedDtoGraphqlApplication.java
@@ -0,0 +1,13 @@
+package id.mydev.generated_dto_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratedDtoGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratedDtoGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/mydev/generated_dto_graphql/GeneratedDtoGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package id.mydev.generated_dto_graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratedDtoGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
